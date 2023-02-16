eda3736a8b8a73e0d25dbc77cdec40a694f29725
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-02-16 17:28:56 -0600

    111: Use profile to determine persistence

--- api/src/main/java/gov/doj/RuntimeObjectFactory.java
+++ api/src/main/java/gov/doj/RuntimeObjectFactory.java
@@ -4,10 +4,27 @@ import gov.doj.adapters.gateways.AzureSqlGateway;
 import gov.doj.adapters.gateways.LocalPersistenceGateway;
 import gov.doj.adapters.presenters.JsonPresenter;
 import gov.doj.usecases.PersistenceGateway;
+import io.quarkus.runtime.configuration.ConfigUtils;
+import io.quarkus.runtime.configuration.ProfileManager;
+
+import java.util.List;
 
 public class RuntimeObjectFactory extends ObjectFactory {
     public static void init() {
         registerObject(Presenter.class, new JsonPresenter());
+        List<String> profiles = ConfigUtils.getProfiles();
+        if (profiles.contains("dev")) {
+            initLocal();
+        } else if (profiles.contains("prod")) {
+            initProd();
+        }
+    }
+
+    private static void initLocal() {
+        registerObject(PersistenceGateway.class, new LocalPersistenceGateway());
+    }
+
+    private static void initProd() {
         registerObject(PersistenceGateway.class, new AzureSqlGateway());
     }
 }
